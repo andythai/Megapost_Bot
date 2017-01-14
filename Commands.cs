0d0a5e4c41f934e1b8381d28fad7153572a5496e
Andy Thai <andy.thai9@gmail.com>
2017-01-14 00:55:15 -0800

    added comments for clarity

---
+++ Commands.cs
@@ -0,0 +1,43 @@
+﻿/** Command class **/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Megapost {
+    class Commands {
+        // Member variables
+        String name;
+        String URL;
+
+        // Public constructor
+        public Commands(String name, String URL) {
+            this.name = name;
+            this.URL = URL;
+        }
+
+        // Setter method for command name
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        // Getter method for command name
+        public String getName() {
+            return name;
+        }
+
+        // Setter method for command URL
+        public void setURL(String URL) {
+            this.URL = URL;
+        }
+
+        // Getter method for command URL
+        public String getURL() {
+            return URL;
+        }
+    }
+}
