7cdbcdd9628da63f8082136821e1e73873bd4088
Andy Thai <andy.thai9@gmail.com>
2017-01-14 01:09:28 -0800

    commented Program.cs and Users.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/* Program class
+This class only serves to initialize the bot.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Megapost {
+    class Program {
+        static void Main(string[] args) {
+            MyBot bot = new MyBot();
+        }
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,15 @@
+﻿/* Users class
+    Planned to be a list of server users for use in tagging for the bot.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megapost {
+
+    class Users {
+    }
+}
